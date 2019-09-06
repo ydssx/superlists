0225bf3548ca7fe63c7f8dcaed752b7901d259bc
ydssx <eric@example.com>
2019-09-06 16:18:17 +0800

    将功能测试变成app,使用LiveServerTestCase

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,9 +1,10 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+from django.test import LiveServerTestCase
 import time
 import unittest
 
-class NewVisitorTest(unittest.TestCase):
+class NewVisitorTest(LiveServerTestCase):
 
     def setUp(self):
         self.browser=webdriver.Firefox()
@@ -19,7 +20,7 @@ class NewVisitorTest(unittest.TestCase):
     def test_can_start_a_list_and_retrieve_it_later(self):
         """一个在线待办事项应用的功能测试"""
         #伊迪丝去看了这个应用的首页
-        self.browser.get('http://localhost:8000')
+        self.browser.get(self.live_server_url)
 
         #测试网页的标题和头部是否包含"To-Do"
         self.assertIn('To-Do',self.browser.title)
@@ -57,6 +58,3 @@ class NewVisitorTest(unittest.TestCase):
 
 
         self.fail('Finish the test!')
-if __name__=='__main__':
-    unittest.main(warnings='ignore')
-    
