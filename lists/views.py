519a61353e29e71b6d36edbe09ee02d670e20fc4
ydssx <eric@example.com>
2019-09-06 14:19:51 +0800

    POST之后重定向，同时在主页模板中显示所有待办事项

--- lists/views.py
+++ lists/views.py
@@ -1,8 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpRequest, HttpResponse
+from lists.models import Item
 
 # Create your views here.
 def home_page(request):
     """主页视图"""
+    if request.method=='POST':
+        Item.objects.create(text=request.POST['item_text'])
+        return redirect('/')
 
-    return render(request,'lists/home.html',{'new_item_text':request.POST.get('item_text', '')})
+    items=Item.objects.all()
+    return render(request,'lists/home.html',{'items':items})
