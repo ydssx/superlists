dc5dfce51451ba02a14737b257f7d372de4a2655
ydssx <eric@example.com>
2019-09-08 17:33:35 +0800

    让表单指向刚添加的新URL

--- lists/views.py
+++ lists/views.py
@@ -5,12 +5,13 @@ from lists.models import Item
 # Create your views here.
 def home_page(request):
     """主页视图"""
-    if request.method=='POST':
-        Item.objects.create(text=request.POST['item_text'])
-        return redirect('/lists/the-only-list-in-the-world/')
     return render(request,'lists/home.html')
 
 def view_list(request):
     """清单视图"""
     items=Item.objects.all()
     return render(request,'lists/list.html',{'items':items})
+
+def new_list(request):
+    Item.objects.create(text=request.POST['item_text'])
+    return redirect('/lists/the-only-list-in-the-world/')
--- superlists/urls.py
+++ superlists/urls.py
@@ -26,4 +26,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('',views.home_page,name='home'),
     path('lists/the-only-list-in-the-world/',views.view_list,name='view_list'),
+    path('lists/new', views.new_list, name='new_list'),
 ]
