19e2bf870efbc6e57e88af4b211c531f4e666cc9
ydssx <eric@example.com>
2019-09-09 11:00:06 +0800

    调整模型，新建迁移

--- lists/views.py
+++ lists/views.py
@@ -1,6 +1,5 @@
 from django.shortcuts import render, redirect
-from django.http import HttpRequest, HttpResponse
-from lists.models import Item
+from lists.models import Item, List
 
 # Create your views here.
 def home_page(request):
@@ -8,10 +7,11 @@ def home_page(request):
     return render(request,'lists/home.html')
 
 def view_list(request):
-    """清单视图"""
+    """待办事项视图"""
     items=Item.objects.all()
     return render(request,'lists/list.html',{'items':items})
 
 def new_list(request):
-    Item.objects.create(text=request.POST['item_text'])
+    list_=List.objects.create()
+    Item.objects.create(text=request.POST['item_text'],list=list_)
     return redirect('/lists/the-only-list-in-the-world/')
